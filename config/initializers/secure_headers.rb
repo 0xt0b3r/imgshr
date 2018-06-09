1491ef91479f9d1535a28f1ae6904083ec5fdc72
henning mueller <mail@nning.io>
2018-06-09 22:33:14 +0200

    Remove dead initializer code

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -5,23 +5,4 @@
   config.x_xss_protection = '1; mode=block'
   config.referrer_policy = 'origin'
   config.csp = SecureHeaders::OPT_OUT
-
-  # if Rails.env.production?
-  #   config.csp = {
-  #     default_src:  %w[https: 'self'],
-  #     script_src:   %w[https: 'self' 'unsafe-eval'],
-  #     style_src:    %w[https: 'self' 'unsafe-inline'],
-  #     img_src:      %w[https: 'self' data:],
-  #     report_uri:   %w[/content_security_policy/forward_report]
-  #   }
-  # else
-  #   # config.csp = SecureHeaders::OPT_OUT
-  #   config.csp = {
-  #     default_src:  %w['self' 0.0.0.0:3001 localhost:3001 localhost:3035],
-  #     script_src:   %w['self' 0.0.0.0:3001 localhost:3001 localhost:3035 'unsafe-eval'],
-  #     style_src:    %w['self' 'unsafe-inline'],
-  #     img_src:      %w['self' data:],
-  #     report_uri:   %w[/content_security_policy/forward_report]
-  #   }
-  # end
 end
